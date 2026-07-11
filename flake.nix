500db00f46ac73cc94f6ca7f66bf22ae772e8224
Jonas Bug <jonas.bug@protonmail.com>
2026-07-12 00:01:25 +0200

    add more valves to dialect + reindex valves, nixify

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "mavlink rapid-dialect";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+  outputs =
+    {
+      rust-overlay,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system}.extend (import rust-overlay);
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        buildInputs =
+          with pkgs;
+          [
+
+          ]
+          ++ [ rustToolchain ];
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = buildInputs ++ [ pkgs.just ];
+        };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
+}
